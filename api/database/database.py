c9883f8492bed4ad057869b9522cd5f5824da430
VJ <nijipavi@gmail.com>
2024-04-20 22:12:43 -0700

    add func current_month_total

--- api/database/database.py
+++ api/database/database.py
@@ -5,6 +5,7 @@ from bson import ObjectId
 from lib.excel import append_data_into_excel, update_data_into_excel
 from lib.settings import settings
 from typing import Generator
+from datetime import datetime
 
 client = MongoClient(settings.databaseURL)
 db = client[settings.databaseName]
@@ -17,21 +18,23 @@ class AuthDB:
     def create_user(cls, data: dict) -> dict:
         res = cls.collection.insert_one(data)
         if not res.inserted_id:
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                detail="Failed to create. Try again...")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Failed to create. Try again...",
+            )
         return cls.get_user_by_id(res.inserted_id)
 
     @classmethod
     def get_user_by_id(cls, object_id: str) -> dict:
-        res:dict = cls.collection.find_one({"_id": ObjectId(object_id)})
-        if isinstance(res,dict):
+        res: dict = cls.collection.find_one({"_id": ObjectId(object_id)})
+        if isinstance(res, dict):
             res["id"] = str(object_id)
         return res
 
     @classmethod
     def get_user_by_username(cls, username: str) -> dict:
         res: dict = cls.collection.find_one({"username": username})
-        if isinstance(res,dict) and res.get("_id"):
+        if isinstance(res, dict) and res.get("_id"):
             res["id"] = str(res["_id"])
             del res["_id"]
         return res
@@ -42,31 +45,39 @@ class ReceiptDB:
 
     @classmethod
     def create_receipt(cls, data: dict):
-        excel_data=data.copy()
+        excel_data = data.copy()
         if excel_data.get("id"):
             del excel_data["id"]
         append_data_into_excel(excel_data, data.get("section_code"))
         res = cls.collection.insert_one(data)
         if not res.inserted_id:
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                detail="Failed to create. Try again...")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Failed to create. Try again...",
+            )
         return cls.get_receipt_by_id(res.inserted_id)
 
     @classmethod
     def update_receipt(cls, receipt_id: str, data: dict):
-        excel_data=data.copy()
+        excel_data = data.copy()
         if excel_data.get("id"):
             del excel_data["id"]
-        update_data_into_excel(data.copy(), data.get("section_code"), data.get("receipt_number"))
+        update_data_into_excel(
+            data.copy(), data.get("section_code"), data.get("receipt_number")
+        )
         res = cls.collection.update_one({"_id": ObjectId(receipt_id)}, {"$set": data})
         if not res.matched_count:
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                detail="Failed to update. Try again...")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Failed to update. Try again...",
+            )
         return cls.get_receipt_by_id(receipt_id)
 
     @classmethod
     def get_receipt_by_receipt_no(cls, receipt_no: str, section_code: str) -> dict:
-        res = cls.collection.find_one({"receipt_number": receipt_no, "section_code": section_code}, {"_id": 0})
+        res = cls.collection.find_one(
+            {"receipt_number": receipt_no, "section_code": section_code}, {"_id": 0}
+        )
         if res:
             res["id"] = str(res["_id"])
             del res["_id"]
@@ -81,8 +92,34 @@ class ReceiptDB:
 
     @classmethod
     def get_all_receipts(cls, section_code: str) -> Generator:
-        cursor: Cursor = cls.collection.find({"section_code": section_code}, {"_id": 0})
+        cursor = cls.collection.find({"section_code": section_code}, {"_id": 0})
         for document in cursor:
             document["id"] = str(document["_id"])
             del document["_id"]
             yield document
+
+    @classmethod
+    def current_month_total(cls, section_code: str) -> float:
+        current_month = datetime.now().month
+
+        start_date = datetime(datetime.now().year, current_month, 1)
+        end_date = datetime(datetime.now().year, current_month + 1, 1)
+
+        pipeline = [
+            {
+                "$match": {
+                    "section_code": section_code,
+                    "date": {"$gte": start_date, "$lt": end_date},
+                }
+            },
+            {
+                "$group": {
+                    "_id": None,
+                    "total_amount": {"$sum": "$donated_amount"},
+                }
+            },
+        ]
+
+        result = list(cls.collection.aggregate(pipeline))
+
+        return result[0]["total_amount"] if result else 0
