cd85cf2f14498b1564bf43a2ac6d33a21d693dbe
Sabareesh98421 <pushpakumar5409@gmail.com>
2024-05-05 12:32:06 +0530

    update:try to bundle

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,49 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// // Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// use tauri::Window;
+// use std::process::Command;
+
+// fn main() {
+//     tauri::Builder::default()
+//         .run(tauri::generate_context!())
+//         .expect("error while running tauri application");
+//         start_fastapi()
+// }
+
+// #[tauri::command]
+// async fn start_fastapi(window: Window) -> Result<(), String> {
+//     let command_result = Command::new("C:/Users/Administrator/Desktop/Projects/next-js-project/api/dist/index.exe").spawn();
+//     if let Err(err) = command_result {
+//         return Err(format!("Error executing Command {}", err));
+//     }
+    
+//     // Show the window
+//     if let Err(err) = window.show() {
+//         return Err(format!("Error showing window: {}", err));
+//     }
+    
+//     Ok(())
+// }
+
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::Command;
+use tauri::Window;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
+    // Call the start_fastapi function after Tauri has been initialized
+    start_fastapi();
+}
+
+#[tauri::command]
+async fn start_fastapi() -> Result<(), String> {
+    let command_result = Command::new("C:/Users/Administrator/Desktop/Projects/next-js-project/api/dist/index.exe").spawn();
+    if let Err(err) = command_result {
+        println!("{}",format!("Error executing Command {}", err));
+    }
+    Ok(())
 }
